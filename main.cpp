53e73aa8b239407f39e26017e4727edafd07e3d4
AliAngel564 <jonathan.aliangel@gmail.com>
2025-04-29 09:05:16 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <conio.h>
+
+
+int main(){
+
+}
+
+void isPrime(int &usrNum)
+{
+    int i = 1;
+    for(i;i<=*usrNum;i++)
+    {
+        if(i == 1 || i==*usrNum)
+        {
+            if
+        }
+    }
+}
