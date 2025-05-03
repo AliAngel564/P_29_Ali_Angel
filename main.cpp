0a9d96cc03077269855baf06f6971f5b443b495f
AliAngel564 <jonathan.aliangel@gmail.com>
2025-05-03 00:10:05 -0600

    continued the code

--- main.cpp
+++ main.cpp
@@ -1,33 +1,87 @@
 
 #include <iostream>
-#include <conio.h>
-#include <cmath>
 
-void getNearestPrime(int &number);
+void getPrimes(int &number);
+void getNearestPrime(int &number,int &downPrime,int &upPrime);
 
 int main(){
-    int num = 5;
-    getNearestPrime(num);
+    int num = 65;
+    getPrimes(num);
 
     return 0;
 }
 
-void getNearestPrime(int &number)
+void getPrimes(int &number)
 {
     int i = number-1;
-    int counter;
-    int nearestPrime = 0;
+    int x = number+1;
+    int downPrime = 0;
+    int upPrime = 0;
+    int firstCounter = 0;
+    int secondCounter = 0;
     
-    for(i;i>0;i--)
+    for(i;i>2;i--)
     {   
-      std::cout<<i<<"\n";
-      int iSqrt = sqrt(i);
-      std::cout<<"sqrt of i: "<<iSqrt<<"\n";
-    for(int j=1;j<=iSqrt;j++)
-    {
-      
+      for(int j=2;j<number;j++)
+      {
+        if(i%j==0)
+        {
+         firstCounter++;
+        }
+      }
+      if(firstCounter == 1)  
+      {
+        downPrime = i;
+        break;
+      }
+      firstCounter = 0;
     }
+    for(x;x>0;x++)
+    {   
+      for(int j=2;j<=x;j++)
+      {
+        if(x%j==0)
+        {
+         secondCounter++;
+        }
+      }
+      if(secondCounter == 1)  
+      {
+       upPrime = x;
+       break;
+      }
+    secondCounter = 0;
     }
+    
+    getNearestPrime(number,downPrime,upPrime);
 
-    std::cout<<"The nearest prime to your number is: "<<nearestPrime;
+}
+
+void getNearestPrime(int &number,int &downPrime,int &upPrime)
+{
+  int downPCounter = 0;
+  int upPCounter = 0;
+  for(downPrime;downPrime<number;downPrime++)
+  {
+    std::cout<<"Downprime: "<<downPrime<<"\n";
+    downPCounter++;
+  }
+  std::cout<<"donwPCounter: "<<downPCounter;
+  for(upPrime;upPrime>number;upPrime--)
+  {
+    std::cout<<"upPrime: "<<upPrime<<"\n";
+    upPCounter++;
+  }
+  std::cout<<"upPCounter: "<<upPCounter;
+  if(upPCounter == downPCounter)
+  {
+    std::cout<<"There's two prime numbers at the same distance from the given number.";
+    std::cout<<"The first prime number is: "<< downPrime << "\nThe second prime number is: "<<upPrime;
+  }else if(downPCounter<upPCounter)
+  {
+    std::cout<<"The nearest prime number is: "<<downPrime;
+  }else if(downPCounter>upPCounter)
+  {
+    std::cout<<"The nearest prime is: "<<upPrime;
+  }
 }
